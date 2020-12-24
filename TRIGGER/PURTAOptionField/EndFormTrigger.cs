419b401e45206d4d9196356211b4d247eda01333
TKFood <tk290@tkfood.com.tw>
2020-12-24 11:27:20 +0800

    PURTAOptionField

---
+++ TRIGGER/PURTAOptionField/EndFormTrigger.cs
@@ -0,0 +1,320 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Configuration;
+using System.Data.Common;
+using System.Data.Odbc;
+using System.Data.OleDb;
+using Ede.Uof.Utility.Data;
+using Ede.Uof.WKF.ExternalUtility;
+using System.Xml;
+
+namespace TKUOF.TRIGGER.PURTAOptionField
+{
+    class EndFormTrigger : ICallbackTriggerPlugin
+    {
+        public class DATAPURTA
+        {
+            public string TaskId;
+
+            public string CREATOR;
+            public string USR_GROUP;
+            public string CREATE_DATE;
+            public string MODIFIER;
+            public string MODI_DATE;
+            public string FLAG;
+            public string CREATE_TIME;
+            public string MODI_TIME;
+            public string TRANS_TYPE;
+            public string TRANS_NAME;
+            public string sync_date;
+            public string sync_time;
+            public string sync_mark;
+            public string sync_count;
+            public string DataUser;
+            public string DataGroup;
+
+            public string TA001;
+            public string TA002;
+            public string TA003;
+            public string TA004;
+            public string TA005;
+            public string TA006;
+            public string TA007;
+            public string TA008;
+            public string TA009;
+            public string TA010;
+            public string TA011;
+            public string TA012;
+            public string TA013;
+            public string TA014;
+            public string TA015;
+            public string TA016;
+            public string TA017;
+            public string TA018;
+            public string TA019;
+            public string TA020;
+            public string TA021;
+            public string TA022;
+            public string TA023;
+            public string TA024;
+            public string TA025;
+            public string TA026;
+            public string TA027;
+            public string TA028;
+            public string TA029;
+            public string TA030;
+            public string TA031;
+            public string TA032;
+            public string TA033;
+            public string TA034;
+            public string TA035;
+            public string TA036;
+            public string TA037;
+            public string TA038;
+            public string TA039;
+            public string TA040;
+            public string TA041;
+            public string TA042;
+            public string TA043;
+            public string TA044;
+            public string TA045;
+            public string TA046;
+            public string UDF01;
+            public string UDF02;
+            public string UDF03;
+            public string UDF04;
+            public string UDF05;
+            public string UDF06;
+            public string UDF07;
+            public string UDF08;
+            public string UDF09;
+            public string UDF10;
+
+        }
+
+        public class DATAPURTB
+        {
+            public string TaskId;
+
+            public string CREATOR;
+            public string USR_GROUP;
+            public string CREATE_DATE;
+            public string MODIFIER;
+            public string MODI_DATE;
+            public string FLAG;
+            public string CREATE_TIME;
+            public string MODI_TIME;
+            public string TRANS_TYPE;
+            public string TRANS_NAME;
+            public string sync_date;
+            public string sync_time;
+            public string sync_mark;
+            public string sync_count;
+            public string DataUser;
+            public string DataGroup;
+            public string TB001;
+            public string TB002;
+            public string TB003;
+            public string TB004;
+            public string TB005;
+            public string TB006;
+            public string TB007;
+            public string TB008;
+            public string TB009;
+            public string TB010;
+            public string TB011;
+            public string TB012;
+            public string TB013;
+            public string TB014;
+            public string TB015;
+            public string TB016;
+            public string TB017;
+            public string TB018;
+            public string TB019;
+            public string TB020;
+            public string TB021;
+            public string TB022;
+            public string TB023;
+            public string TB024;
+            public string TB025;
+            public string TB026;
+            public string TB027;
+            public string TB028;
+            public string TB029;
+            public string TB030;
+            public string TB031;
+            public string TB032;
+            public string TB033;
+            public string TB034;
+            public string TB035;
+            public string TB036;
+            public string TB037;
+            public string TB038;
+            public string TB039;
+            public string TB040;
+            public string TB041;
+            public string TB042;
+            public string TB043;
+            public string TB044;
+            public string TB045;
+            public string TB046;
+            public string TB047;
+            public string TB048;
+            public string TB049;
+            public string TB050;
+            public string TB051;
+            public string TB052;
+            public string TB053;
+            public string TB054;
+            public string TB055;
+            public string TB056;
+            public string TB057;
+            public string TB058;
+            public string TB059;
+            public string TB060;
+            public string TB061;
+            public string TB062;
+            public string TB063;
+            public string TB064;
+            public string TB065;
+            public string TB066;
+            public string TB067;
+            public string TB068;
+            public string TB069;
+            public string TB070;
+            public string TB071;
+            public string TB072;
+            public string TB073;
+            public string TB074;
+            public string TB075;
+            public string TB076;
+            public string TB077;
+            public string TB078;
+            public string TB079;
+            public string TB080;
+            public string TB081;
+            public string TB082;
+            public string TB083;
+            public string TB084;
+            public string TB085;
+            public string TB086;
+            public string TB087;
+            public string TB088;
+            public string TB089;
+            public string TB090;
+            public string TB091;
+            public string TB092;
+            public string TB093;
+            public string TB094;
+            public string TB095;
+            public string TB096;
+            public string TB097;
+            public string TB098;
+            public string TB099;
+            public string UDF01;
+            public string UDF02;
+            public string UDF03;
+            public string UDF04;
+            public string UDF05;
+            public string UDF06;
+            public string UDF07;
+            public string UDF08;
+            public string UDF09;
+            public string UDF10;
+
+        }
+
+
+        public void Finally()
+        {
+            
+        }
+
+        public string GetFormResult(ApplyTask applyTask)
+        {
+            DATAPURTA DataPURTA = new DATAPURTA();
+
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(applyTask.CurrentDocXML);
+
+            //MA001 = applyTask.Task.CurrentDocument.Fields["MA001"].FieldValue.ToString().Trim();
+            //MA002 = applyTask.Task.CurrentDocument.Fields["MA002"].FieldValue.ToString().Trim();
+
+            XmlNode node = xmlDoc.SelectSingleNode("./Form/FormFieldValue/FieldItem[@fieldId='PURTAB']");
+
+            DataPURTA.TaskId = applyTask.Task.TaskId;
+            DataPURTA.TA001 = "A312";
+            DataPURTA.TA002 = "20201224099";
+            DataPURTA.TA004 = node.SelectSingleNode("FieldValue").Attributes["DEP"].Value;
+            DataPURTA.TA006 = node.SelectSingleNode("FieldValue").Attributes["COMMENT"].Value;
+            DataPURTA.TA012 = node.SelectSingleNode("FieldValue").Attributes["NAME"].Value;
+
+            if (applyTask.FormResult == Ede.Uof.WKF.Engine.ApplyResult.Adopt)
+            {
+                if (!string.IsNullOrEmpty(DataPURTA.TA001) && !string.IsNullOrEmpty(DataPURTA.TA002))
+                {
+                    INSERTINTOPURTAB(DataPURTA);
+                }
+            }
+
+            return "";
+        }
+
+        public void OnError(Exception errorException)
+        {
+            
+        }
+
+        public void INSERTINTOPURTAB(DATAPURTA DataPURTA)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["ERPconnectionstring"].ToString();
+
+            StringBuilder queryString = new StringBuilder();
+            queryString.AppendFormat(@"
+                                       INSERT INTO [TK].dbo.PURTA
+                                        (TA001,TA002,TA004,TA006,TA012)
+                                        VALUES
+                                        (@TA001,@TA002,@TA004,@TA006,@TA012) 
+                                    ");
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+
+                    SqlCommand command = new SqlCommand(queryString.ToString(), connection);
+                    command.Parameters.Add("@TA001", SqlDbType.NVarChar).Value = DataPURTA.TA001;
+                    command.Parameters.Add("@TA002", SqlDbType.NVarChar).Value = DataPURTA.TA002;
+                    command.Parameters.Add("@TA004", SqlDbType.NVarChar).Value = DataPURTA.TA004;
+                    command.Parameters.Add("@TA006", SqlDbType.NVarChar).Value = DataPURTA.TA006;
+                    command.Parameters.Add("@TA012", SqlDbType.NVarChar).Value = DataPURTA.TA012;
+
+                    command.Connection.Open();
+
+                    int count = command.ExecuteNonQuery();
+
+                    connection.Close();
+                    connection.Dispose();
+
+                }
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+
+            }
+
+        }
+    }
+
+}
+
