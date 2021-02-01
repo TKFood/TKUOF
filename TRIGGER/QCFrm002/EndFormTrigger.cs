766b68278e39ba3182e8fcd60cab1675c4efe5dd
TKFood <tk290@tkfood.com.tw>
2021-02-01 17:13:07 +0800

    version

--- TRIGGER/QCFrm002/EndFormTrigger.cs
+++ TRIGGER/QCFrm002/EndFormTrigger.cs
@@ -54,7 +54,7 @@ namespace TKUOF.TRIGGER.QCFrm002
             //測試的id
             //Form.SetAttribute("formVersionId", "83a22122-9d93-4603-bf70-489bd4e48cfb");
             //正式的id
-            Form.SetAttribute("formVersionId", "88ac00aa-f150-4695-bd49-8bede4e4c86d");
+            Form.SetAttribute("formVersionId", "6a6dff6e-184a-4f34-846f-8dac406351cd");
             Form.SetAttribute("urgentLevel", "2");
             //加入節點底下
             xmlDoc.AppendChild(Form);
