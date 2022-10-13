4a62f3df84e0675c14630aed4ace4402afbe9a75
TKFood <tk290@tkfood.com.tw>
2022-10-13 15:26:37 +0800

    COPTAB

--- TRIGGER/COPTAB/EndFormTrigger.cs
+++ TRIGGER/COPTAB/EndFormTrigger.cs
@@ -28,27 +28,22 @@ namespace TKUOF.TRIGGER.COPTAB
 
         public string GetFormResult(ApplyTask applyTask)
         {
-            string TC001 = null;
-            string TC002 = null;
+            string TA001 = null;
+            string TA002 = null;
             string FORMID = null;
             string MODIFIER = null;
-            string MOC = null;
-            string PUR = null;
-            string TC040 = null;
+     
             UserUCO userUCO = new UserUCO();
 
             XmlDocument xmlDoc = new XmlDocument();
             xmlDoc.LoadXml(applyTask.CurrentDocXML);
-            TC001 = applyTask.Task.CurrentDocument.Fields["TC001"].FieldValue.ToString().Trim();
-            TC002 = applyTask.Task.CurrentDocument.Fields["TC002"].FieldValue.ToString().Trim();
-            MOC = applyTask.Task.CurrentDocument.Fields["MOC"].FieldValue.ToString().Trim();
-            PUR = applyTask.Task.CurrentDocument.Fields["PUR"].FieldValue.ToString().Trim();
+            TA001 = applyTask.Task.CurrentDocument.Fields["TA001"].FieldValue.ToString().Trim();
+            TA002 = applyTask.Task.CurrentDocument.Fields["TA002"].FieldValue.ToString().Trim();         
             FORMID = applyTask.FormNumber;
             //MODIFIER = applyTask.Task.Applicant.Account;
       
             //取得簽核人工號
-            EBUser ebUser = userUCO.GetEBUser(applyTask.Task.CurrentSite.CurrentNode.ActualSignerId);
-            TC040 = ebUser.Account;
+            EBUser ebUser = userUCO.GetEBUser(applyTask.Task.CurrentSite.CurrentNode.ActualSignerId);          
             MODIFIER = ebUser.Account;
 
 
@@ -57,9 +52,9 @@ namespace TKUOF.TRIGGER.COPTAB
             ///核準 == Ede.Uof.WKF.Engine.ApplyResult.Adopt
             if (applyTask.SignResult == Ede.Uof.WKF.Engine.SignResult.Approve)
             {
-                if (!string.IsNullOrEmpty(TC001) && !string.IsNullOrEmpty(TC002))
+                if (!string.IsNullOrEmpty(TA001) && !string.IsNullOrEmpty(TA002))
                 {
-                    UPDATECOPTCD(TC001, TC002, FORMID, MODIFIER, MOC, PUR, TC040);
+                    UPDATECOPTAB(TA001, TA002, FORMID, MODIFIER);
                 }
             }
 
@@ -72,7 +67,7 @@ namespace TKUOF.TRIGGER.COPTAB
 
         }
 
-        public void UPDATECOPTCD(string TC001, string TC002, string FORMID, string MODIFIER, string MOC, string PUR,string TC040)
+        public void UPDATECOPTAB(string TA001, string TA002, string FORMID, string MODIFIER)
         {
             string TC027 = "Y";
             string TC048 = "N";
@@ -84,35 +79,39 @@ namespace TKUOF.TRIGGER.COPTAB
             string connectionString = ConfigurationManager.ConnectionStrings["ERPconnectionstring"].ToString();
 
 
-            if (!string.IsNullOrEmpty(MOC))
-            {
-                MOC = DateTime.Now.ToString("MM/dd") + ":" + MOC + " ";
-            }
-            if (!string.IsNullOrEmpty(PUR))
-            {
-                PUR = DateTime.Now.ToString("MM/dd") + ":" + PUR + " ";
-            }
+         
 
             StringBuilder queryString = new StringBuilder();
             queryString.AppendFormat(@"
-                                    UPDATE [TK].dbo.COPTC
-                                    SET TC027=@TC027,TC048=@TC048, FLAG=FLAG+1,COMPANY=@COMPANY,MODIFIER=@MODIFIER ,MODI_DATE=@MODI_DATE, MODI_TIME=@MODI_TIME 
-                                    ,UDF03=@FORMID
-                                    ,UDF05=SUBSTRING((UDF05+' '+@MOC+' '+@PUR+' '),1,250)
-                                    ,TC040=@TC040
+                                        UPDATE [TK].dbo.COPTB
+                                        SET TB011=@TB011
+                                        ,FLAG=FLAG+1,MODIFIER=@MODIFIER,MODI_DATE=@MODI_DATE,MODI_TIME=@MODI_TIME
+                                        WHERE TB001=@TB001 AND TB002=@TB002
 
-                                    WHERE TC001=@TC001 AND TC002=@TC002
+                                        UPDATE [TK].dbo.COPTA
+                                        SET TA016=@TA016,TA019=@TA019,TA029=@TA029
+                                        ,FLAG=FLAG+1,MODIFIER=@MODIFIER,MODI_DATE=@MODI_DATE,MODI_TIME=@MODI_TIME
+                                        ,UDF02=@UDF02
+                                        WHERE TA001=@TA001 AND TA002=@TA002
 
-                                    UPDATE [TK].dbo.COPTD 
-                                    SET TD021=@TD021, FLAG=FLAG+1,COMPANY=@COMPANY,MODIFIER=@MODIFIER ,MODI_DATE=@MODI_DATE, MODI_TIME=@MODI_TIME 
-                                    WHERE TD001=@TC001 AND TD002=@TC002
+                                        INSERT INTO [TK].dbo.COPMB
+                                        (MB001, MB002, MB003, MB004, MB005, MB007, MB008, MB009,  MB010, MB012, MB013, MB014, MB015, MB016, MB017, MB018,  COMPANY ,CREATOR ,USR_GROUP ,CREATE_DATE ,FLAG) 
+                                        SELECT TA004 MB001,TB004 MB002,TB008 MB003,TA007 MB004,'' MB005,TB013 MB007,TB009 MB008,TB016 MB009,''  MB010,'' MB012,(CASE WHEN TA022='1' THEN 'Y' ELSE 'N' END) MB013,'' MB014,0 MB015,0 MB016,TB016 MB017,'' MB018,'TK'  COMPANY ,TA005 CREATOR ,MV004 USR_GROUP ,CONVERT(NVARCHAR,GETDATE(),112) CREATE_DATE ,1 FLAG
+                                        FROM [TK].dbo.COPTA,[TK].dbo.COPTB,[TK].dbo.CMSMV
+                                        WHERE 1=1
+                                        AND TA001=TB001 AND TA002=TB002
+                                        AND MV001=TA005
+                                        AND TA001=@TA001 AND TA002=@TA002
 
-
-                                    UPDATE [TK].dbo.COPTC
-                                    SET TC040=[ACCOUNT]
-                                    FROM [TKIT].[dbo].[UOFACCOUNTS]
-                                    WHERE [FORMNAME]='訂單'
-                                    AND TC001=@TC001 AND TC002=@TC002
+                                        INSERT INTO [TK].dbo.COPMC
+                                        (MC001, MC002, MC003, MC004, MC005, MC006, MC007, MC008, MC009,  COMPANY ,CREATOR ,USR_GROUP ,CREATE_DATE ,FLAG) 
+                                        SELECT TA004 MC001,TB004 MC002,TB008 MC003,TA007 MC004,TK004 MC005,TK006 MC006,'' MC007,TK005 MC008,TB016 MC009,'TK'  COMPANY ,TA005 CREATOR ,MV004 USR_GROUP ,CONVERT(NVARCHAR,GETDATE(),112) CREATE_DATE ,1 FLAG
+                                        FROM [TK].dbo.COPTA,[TK].dbo.COPTB,[TK].dbo.CMSMV,[TK].dbo.COPTK 
+                                        WHERE 1=1
+                                        AND TA001=TB001 AND TA002=TB002
+                                        AND MV001=TA005
+                                        AND TK001=TB001 AND TK002=TB002 AND TK003=TB003
+                                        AND TA001=@TA001 AND TA002=@TA002
 
                                         ");
 
@@ -122,19 +121,18 @@ namespace TKUOF.TRIGGER.COPTAB
                 {
 
                     SqlCommand command = new SqlCommand(queryString.ToString(), connection);
-                    command.Parameters.Add("@TC001", SqlDbType.NVarChar).Value = TC001;
-                    command.Parameters.Add("@TC002", SqlDbType.NVarChar).Value = TC002;
-                    command.Parameters.Add("@FORMID", SqlDbType.NVarChar).Value = FORMID;
-                    command.Parameters.Add("@TC027", SqlDbType.NVarChar).Value = TC027;
-                    command.Parameters.Add("@TC048", SqlDbType.NVarChar).Value = TC048;
-                    command.Parameters.Add("@TD021", SqlDbType.NVarChar).Value = TD021;
-                    command.Parameters.Add("@COMPANY", SqlDbType.NVarChar).Value = COMPANY;
+                    command.Parameters.Add("@TA001", SqlDbType.NVarChar).Value = TA001;
+                    command.Parameters.Add("@TA002", SqlDbType.NVarChar).Value = TA002;
+                    command.Parameters.Add("@TB001", SqlDbType.NVarChar).Value = TA001;
+                    command.Parameters.Add("@TB002", SqlDbType.NVarChar).Value = TA002;
+                    command.Parameters.Add("@TB011", SqlDbType.NVarChar).Value = "Y";
+                    command.Parameters.Add("@TA016", SqlDbType.NVarChar).Value = "Y";
+                    command.Parameters.Add("@TA019", SqlDbType.NVarChar).Value = "Y";
+                    command.Parameters.Add("@TA029", SqlDbType.NVarChar).Value = "N";
                     command.Parameters.Add("@MODIFIER", SqlDbType.NVarChar).Value = MODIFIER;
-                    command.Parameters.Add("@MODI_DATE", SqlDbType.NVarChar).Value = MODI_DATE;
-                    command.Parameters.Add("@MODI_TIME", SqlDbType.NVarChar).Value = MODI_TIME;
-                    command.Parameters.Add("@MOC", SqlDbType.NVarChar).Value = MOC;
-                    command.Parameters.Add("@PUR", SqlDbType.NVarChar).Value = PUR;
-                    command.Parameters.Add("@TC040", SqlDbType.NVarChar).Value = TC040;
+                    command.Parameters.Add("@MODI_DATE", SqlDbType.NVarChar).Value = DateTime.Now.ToString("yyyyMMdd");
+                    command.Parameters.Add("@MODI_TIME", SqlDbType.NVarChar).Value = DateTime.Now.ToString("HH:mm:ss");
+                    command.Parameters.Add("@UDF02", SqlDbType.NVarChar).Value = FORMID;
 
                     command.Connection.Open();
 
