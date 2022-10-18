283f3f0c1d17dedba5abbe07d5a64ff8a14c4d07
TKFood <tk290@tkfood.com.tw>
2022-10-18 14:03:26 +0800

    UPDATEASTMBASTMC

--- TRIGGER/ASTI02/EndFormTrigger.cs
+++ TRIGGER/ASTI02/EndFormTrigger.cs
@@ -96,9 +96,53 @@ namespace TKUOF.TRIGGER.ASTI02
                                         ,UDF02=@UDF02
                                         WHERE MB001=@MB001
 
+                                        DELETE [TK].dbo.ASTTD
+                                        WHERE TD001+TD002 IN (SELECT MB042+MB043 FROM [TK].dbo.ASTMB WHERE MB001=@MB001)
+
+                                        DELETE [TK].dbo.ASTTC
+                                        WHERE TC001+TC002 IN (SELECT MB042+MB043 FROM [TK].dbo.ASTMB WHERE MB001=@MB001)
 
 
+                                        INSERT INTO [TK].dbo.ASTTC
+                                        (
+                                        TC001,TC002,TC003,TC004,TC005,TC006,TC007,TC008,TC009,TC010, 
+                                        TC011,TC012,TC013,TC014,TC015,TC016,TC017,TC018,TC019,TC020,
+                                        TC021,TC022,TC025,TC027,TC028,TC029,TC030,
+                                        TC031,TC032,TC033,TC035,TC036,TC037,TC038,TC039, 
+                                        TC068,TC069,
+                                        TC073,TC074,TC075,TC076,TC077,TC078,TC079,TC080,
+                                        TC086,TC088,TC092,TC093, 
+                                        COMPANY,CREATOR,USR_GROUP,CREATE_DATE ,FLAG,
+                                        CREATE_TIME,MODI_TIME,TRANS_TYPE,TRANS_NAME
+                                        )
+                                        SELECT 
+                                        MB042 TC001,MB043 TC002,MB016 TC003,MB001 TC004,MB012 TC005,MB020 TC006,MB021 TC007,MB029 TC008,MB015 TC009,MB022 TC010, 
+                                        MB052 TC011,MB053 TC012,MB032 TC013,'C0' TC014,'Y' TC015,0 TC016,'' TC017,'' TC018,MB018 TC019,MB080 TC020,
+                                        0 TC021,0 TC022,'N' TC025,MB016 TC027,'AD' TC028,0 TC029,0 TC030,
+                                        'N' TC031,'N' TC032,MB051 TC033,MB014 TC035,MB058 TC036,MB026 TC037,MB041 TC038,0 TC039, 
+                                        MB076 TC068,'N' TC069,
+                                        MB069 TC073,MB068 TC074,MB073 TC075,MB074 TC076, MB075 TC077,MB064 TC078,MB066 TC079,MB032 TC080,
+                                        0 TC086,0 TC088,MB078 TC092,MB079 TC093, 
+                                        'TK' COMPANY ,ASTMB.CREATOR CREATOR ,ASTMB.USR_GROUP USR_GROUP ,CONVERT(NVARCHAR,GETDATE(),112) CREATE_DATE ,0 FLAG,
+                                        CONVERT(NVARCHAR,GETDATE(),108) CREATE_TIME,CONVERT(NVARCHAR,GETDATE(),108)  MODI_TIME, 'P004' TRANS_TYPE,'ASTI02' TRANS_NAME
+                                        FROM [TK].dbo.ASTMB
+                                        WHERE MB001=@MB001
 
+                                        INSERT INTO [TK].dbo.ASTTD
+                                        (
+                                        TD001,TD002,TD003,TD004,TD005,TD006,TD007, TD008, TD009,TD010,TD016, 
+                                        COMPANY,CREATOR,USR_GROUP,CREATE_DATE ,FLAG, CREATE_TIME
+                                        )
+                                        SELECT 
+                                        MB042 TD001,MB043 TD002,MC002 TD003,MC003 TD004,MC004 TD005,MB031 TD006,0 TD007,'' TD008,'Y' TD009,MB001 TD010,MB066 TD016, 
+                                        'TK' COMPANY ,ASTMB.CREATOR CREATOR ,ASTMB.USR_GROUP USR_GROUP ,CONVERT(NVARCHAR,GETDATE(),112) CREATE_DATE ,0 FLAG,
+                                        CONVERT(NVARCHAR,GETDATE(),108) CREATE_TIME
+                                        FROM [TK].dbo.ASTMB,[TK].dbo.ASTMC
+                                        WHERE 1=1
+                                        AND MB001=MC001
+                                        AND MB001=@MB001
+
+ 
                                         ");
 
             try
@@ -108,7 +152,7 @@ namespace TKUOF.TRIGGER.ASTI02
 
                     SqlCommand command = new SqlCommand(queryString.ToString(), connection);
                     command.Parameters.Add("@MB001", SqlDbType.NVarChar).Value = MB001;
-                    command.Parameters.Add("@MB047", SqlDbType.NVarChar).Value = DateTime.Now.ToString("yyyyMMdd");
+                    command.Parameters.Add("@MB047", SqlDbType.NVarChar).Value = MB016;
                     command.Parameters.Add("@MB048", SqlDbType.NVarChar).Value = MODIFIER;
                     command.Parameters.Add("@MB042", SqlDbType.NVarChar).Value = MB042;
                     command.Parameters.Add("@MB043", SqlDbType.NVarChar).Value = MB043;
