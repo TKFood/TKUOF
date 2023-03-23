986b352833449afd0952e3db64924a6c592af3cb
TKFood <tk290@tkfood.com.tw>
2023-03-23 17:19:30 +0800

    MODIFY

---
+++ TRIGGER/ACTI10/EndFormTrigger.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Configuration;
+using System.Data.Common;
+using System.Data.Odbc;
+using System.Data.OleDb;
+using Ede.Uof.Utility.Data;
+using Ede.Uof.WKF.ExternalUtility;
+using System.Xml;
+using Ede.Uof.EIP.Organization.Util;
+using Ede.Uof.EIP.SystemInfo;
+
+namespace TKUOF.TRIGGER.ACTI10
+{
+    //ACRI02.結帳單 的核準
+
+
+    class EndFormTrigger : ICallbackTriggerPlugin
+    {
+        public void Finally()
+        {
+
+        }
+
+        public string GetFormResult(ApplyTask applyTask)
+        {
+            string TA001 = null;
+            string TA002 = null;
+            string FORMID = null;
+            string MODIFIER = null;
+            UserUCO userUCO = new UserUCO();
+
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(applyTask.CurrentDocXML);
+            TA001 = applyTask.Task.CurrentDocument.Fields["TA001"].FieldValue.ToString().Trim();
+            TA002 = applyTask.Task.CurrentDocument.Fields["TA002"].FieldValue.ToString().Trim();
+
+            FORMID = applyTask.FormNumber;
+            //MODIFIER = applyTask.Task.Applicant.Account;
+
+            //取得簽核人工號
+            EBUser ebUser = userUCO.GetEBUser(Current.UserGUID);
+            MODIFIER = ebUser.Account;
+
+           
+            ///核準 == Ede.Uof.WKF.Engine.ApplyResult.Adopt
+            if (applyTask.SignResult == Ede.Uof.WKF.Engine.SignResult.Approve)
+            {
+                if (!string.IsNullOrEmpty(TA001) && !string.IsNullOrEmpty(TA002) )
+                {
+                    UPDATE_ACTI10(TA001, TA002, FORMID, MODIFIER);
+                }
+            }
+
+
+            return "";
+        }
+
+        public void OnError(Exception errorException)
+        {
+
+        }
+
+        public void UPDATE_ACTI10(string TA001, string TA002, string FORMID, string MODIFIER)
+        {
+            string TA014 = DateTime.Now.ToString("yyyyMMdd");
+            string TA010 = "Y";
+            string TA015 = MODIFIER;
+            string TA016 = "N";
+            string TA018 = "N";
+            string TB016 = "Y";
+
+            string COMPANY = "TK";
+            string MODI_DATE = DateTime.Now.ToString("yyyyMMdd");
+            string MODI_TIME = DateTime.Now.ToString("HH:mm:dd");
+
+            string connectionString = ConfigurationManager.ConnectionStrings["ERPconnectionstring"].ToString();
+
+
+
+            StringBuilder queryString = new StringBuilder();
+            queryString.AppendFormat(@"       
+                                    
+                                    UPDATE [test0923].dbo.ACTTA
+                                    SET 
+                                    TA014=@TA014 
+                                    ,TA010=@TA010 
+                                    ,TA015=@TA015 
+                                    ,TA016=@TA016 
+                                    ,TA018=@TA018 
+                                    ,FLAG=FLAG+1
+                                    ,MODIFIER=@MODIFIER 
+                                    ,MODI_DATE=@MODI_DATE
+                                    ,MODI_TIME=@MODI_TIME
+                                    WHERE TA001=@TA001 AND TA002=TA002
+
+                                    UPDATE [test0923].dbo.ACTTB
+                                    SET 
+                                    TB016=@TB016 
+                                    ,FLAG=FLAG+1
+                                    ,MODIFIER=@MODIFIER 
+                                    ,MODI_DATE=@MODI_DATE
+                                    ,MODI_TIME=@MODI_TIME
+                                    WHERE TB001=@TB001 AND TB002=TB002
+
+                                        ");
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+
+                    SqlCommand command = new SqlCommand(queryString.ToString(), connection);
+                    command.Parameters.Add("@TA001", SqlDbType.NVarChar).Value = TA001;
+                    command.Parameters.Add("@TA002", SqlDbType.NVarChar).Value = TA002;
+
+                    command.Parameters.Add("@TB001", SqlDbType.NVarChar).Value = TA001;
+                    command.Parameters.Add("@TB002", SqlDbType.NVarChar).Value = TA002;
+                    command.Parameters.Add("@TA014", SqlDbType.NVarChar).Value = TA014;
+                    command.Parameters.Add("@TA010", SqlDbType.NVarChar).Value = TA010;
+                    command.Parameters.Add("@TA015", SqlDbType.NVarChar).Value = TA015;
+                    command.Parameters.Add("@TA016", SqlDbType.NVarChar).Value = TA016;
+                    command.Parameters.Add("@TA018", SqlDbType.NVarChar).Value = TA018;
+                    command.Parameters.Add("@TB016", SqlDbType.NVarChar).Value = TB016;
+           
+                    command.Parameters.Add("@MODIFIER", SqlDbType.NVarChar).Value = MODIFIER;
+                    command.Parameters.Add("@MODI_DATE", SqlDbType.NVarChar).Value = DateTime.Now.ToString("yyyyMMdd");
+                    command.Parameters.Add("@MODI_TIME", SqlDbType.NVarChar).Value = DateTime.Now.ToString("HH:mm:ss");
+                    command.Parameters.Add("@UDF02", SqlDbType.NVarChar).Value =FORMID;
+
+
+
+                    command.Connection.Open();
+
+                    int count = command.ExecuteNonQuery();
+
+                    connection.Close();
+                    connection.Dispose();
+
+                }
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+
+            }
+
+
+
+        }
+
+
+      
+    }
+}
