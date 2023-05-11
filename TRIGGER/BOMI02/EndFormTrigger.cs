94795038176a852df9dfb35540a6d38c73cfe136
TKFood <tk290@tkfood.com.tw>
2023-05-11 10:49:10 +0800

    BOMI02

---
+++ TRIGGER/BOMI02/EndFormTrigger.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Configuration;
+using System.Data.Common;
+using System.Data.Odbc;
+using System.Data.OleDb;
+using Ede.Uof.Utility.Data;
+using Ede.Uof.WKF.ExternalUtility;
+using System.Xml;
+using Ede.Uof.EIP.Organization.Util;
+using Ede.Uof.EIP.SystemInfo;
+
+namespace TKUOF.TRIGGER.BOMI02
+{
+    //ERP-BOM02.BOM表 的 核準
+
+
+    class EndFormTrigger : ICallbackTriggerPlugin
+    {
+        public void Finally()
+        {
+
+        }
+
+        public string GetFormResult(ApplyTask applyTask)
+        {
+            string MC001 = null;
+
+            string FORMID = null;
+            string MODIFIER = null;
+            UserUCO userUCO = new UserUCO();
+
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(applyTask.CurrentDocXML);
+            MC001 = applyTask.Task.CurrentDocument.Fields["MC001"].FieldValue.ToString().Trim();
+           
+
+            FORMID = applyTask.FormNumber;
+            //MODIFIER = applyTask.Task.Applicant.Account;
+
+            //取得簽核人工號
+            EBUser ebUser = userUCO.GetEBUser(Current.UserGUID);
+            MODIFIER = ebUser.Account;
+
+           
+            ///核準 == Ede.Uof.WKF.Engine.ApplyResult.Adopt
+            if (applyTask.SignResult == Ede.Uof.WKF.Engine.SignResult.Approve)
+            {
+                if (!string.IsNullOrEmpty(MC001) )
+                {
+                    UPDATE_BOMI02(MC001, FORMID, MODIFIER);
+                }
+            }
+
+
+            return "";
+        }
+
+        public void OnError(Exception errorException)
+        {
+
+        }
+
+        public void UPDATE_BOMI02(string MC001, string FORMID, string MODIFIER)
+        {           
+            string connectionString = ConfigurationManager.ConnectionStrings["ERPconnectionstring"].ToString();
+
+            string UDF01 = MODIFIER+"，已簽核:" +DateTime.Now.ToString("yyyyMMdd HH:mm:ss");
+            string UDF02 = FORMID;
+
+            string COMPANY = "TK";
+            string MODI_DATE = DateTime.Now.ToString("yyyyMMdd");
+            string MODI_TIME = DateTime.Now.ToString("HH:mm:dd");
+
+            StringBuilder queryString = new StringBuilder();
+            queryString.AppendFormat(@"   
+
+                                        UPDATE  [TK].dbo.BOMMC
+                                        SET
+                                        UDF01=@UDF01
+                                        ,UDF02=@UDF02
+                                        WHERE MC001=@MC001 
+
+                                        ");
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+
+                    SqlCommand command = new SqlCommand(queryString.ToString(), connection);
+                    command.Parameters.Add("@MC001", SqlDbType.NVarChar).Value = MC001;
+                    command.Parameters.Add("@UDF01", SqlDbType.NVarChar).Value = UDF01;
+                    command.Parameters.Add("@UDF02", SqlDbType.NVarChar).Value = UDF02;
+
+                    command.Connection.Open();
+
+                    int count = command.ExecuteNonQuery();
+
+                    connection.Close();
+                    connection.Dispose();
+
+                }
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+
+            }
+
+
+
+        }
+
+
+      
+    }
+}
